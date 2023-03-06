26cba3ecc547e587ca11cf63329974115e0b04e7
saurabh <baghelsaurabh687@gmail.com>
2023-03-06 19:39:56 +0530

    day6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   String name="";
+  bool setButton=false;
   Widget build(BuildContext context) {
     return  Material(
       color: Colors.white,
@@ -25,6 +26,7 @@ class _LoginPageState extends State<LoginPage> {
        ),
        ),
         const SizedBox(height: 20,),
+
          Padding(padding: EdgeInsets.symmetric(vertical: 10, horizontal: 32),
         child: Column(
           children: [
@@ -49,11 +51,45 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
          const SizedBox(height: 20,),
-         ElevatedButton(onPressed:(){
-                  Navigator.pushNamed(context, MyRoutes.homeRoute);  // we passed call back function to this as we pressed it back the given function
-         }, child: Text("login"),
-        style: TextButton.styleFrom(minimumSize: Size(130,40)),// ab hm cha rhe hain ki jese hi login pr click ho to hm homepage pr chle jaye
-        )
+         InkWell(    //inkwell hme onTap function dega jiska kam tap krne pr action lena hoga
+          onTap: () async{ 
+            setState(() {
+              setButton=true;
+             
+            }); 
+            await Future.delayed(Duration(seconds:1));  // ye hmare login button pr click hone ke 1 sec bad hme dusre page pr le jayega
+            Navigator.pushNamed(context, MyRoutes.homeRoute);
+            
+          },
+         child:AnimatedContainer( 
+          duration: Duration(seconds: 1), // hm container ka use kr ke buttun bna rhe hain to is pr hm click nhi kr payenge isiliye hm InkWell ka use krenge
+          width:60,
+          height:40,
+           alignment: Alignment.center,
+           child:setButton?Icon(   // ye login button ko setbutton true yani press hone pr tick wale sign se change kr dega
+            Icons.done,
+           color: Colors.white,
+           )
+                                            //   color:Colors.deepPurple,ise hmne hta diya ku ki hm ise decoration me de rhe hain or ek bar me hm do jagah nhi de skte
+          : Text(
+            "Login",
+               style:TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,  
+               )
+               ),
+            decoration: BoxDecoration(
+            color:Colors.deepPurple,
+            borderRadius: BorderRadius.circular(20)
+          ),
+
+         ),
+         )
+       // ElevatedButton(onPressed:(){
+           //       Navigator.pushNamed(context, MyRoutes.homeRoute);  // we passed call back function to this as we pressed it back the given function
+       //  }, child: Text("login"),
+      //  style: TextButton.styleFrom(minimumSize: Size(130,40)),// ab hm cha rhe hain ki jese hi login pr click ho to hm homepage pr chle jaye
+      //  )
           ],
         ),
        
