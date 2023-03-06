208529effa52c3811c1341d74a69235d65f1ba03
saurabh <baghelsaurabh687@gmail.com>
2023-03-06 11:52:19 +0530

    day 4 complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 //import 'package:flutter/cupertino.dart';
 import 'package:flimazon/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/home_page.dart';
 
@@ -17,17 +18,19 @@ void main() {
   Widget build(BuildContext context) {
     return   MaterialApp(    
      themeMode: ThemeMode.light,
-     theme: ThemeData(primarySwatch: Colors.deepPurple),
-     darkTheme: ThemeData(
+    theme: ThemeData(primarySwatch: Colors.deepPurple,
+    ),
+    darkTheme: ThemeData(
       brightness: Brightness.light,
     
      ),
-     initialRoute: "/home",
+     initialRoute: "/",
      routes: {
    "/":(context) => const LoginPage(), // hmne route me / lga kr homepage ko default bna diya hai,or hmne upr home m bhi home page hi diya tha
-      "/home":(context) => const HomePage() ,                            //to dono me se hme ek hi jgh rkhna hai dusra htana pdega...route ka mtlb hota hai konsa page kb open hoga mtlb routing of pages
+    "/home":(context) => const HomePage() ,                            //to dono me se hme ek hi jgh rkhna hai dusra htana pdega...route ka mtlb hota hai konsa page kb open hoga mtlb routing of pages
     "/login":(context) => const LoginPage()// ye LoginPage class ka object hai iske aage hm new keyword bhi use kr skte hain
      },                              
      );
   }
 }
+//
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,14 +5,48 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child:Center(
-        child: Text("Login to this App",style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold
-        ),),
+    return  Material(
+      color: Colors.white,
+      child: Column(
+       children: [Image.asset("assets/login.png",fit: BoxFit.cover,),
+        const SizedBox(height: 20,),// use to provide space of given height b/w image and text(welcome)
+       const Text("Welcome",
+       style: TextStyle(
+        fontSize: 22,
+        fontWeight: FontWeight.bold
+       ),
+       ),
+        const SizedBox(height: 20,),
+         Padding(padding: EdgeInsets.symmetric(vertical: 10, horizontal: 32),
+        child: Column(
+          children: [
+            TextFormField(
+          decoration: const InputDecoration (
+            hintText: "Enter User Name",
+            labelText:("Username"),
+          ),
         ),
+         TextFormField( 
+          obscureText: true,//this is used to hide the password
+          decoration: const InputDecoration(
+            hintText: "Enter PassWord",
+            labelText:("Password"),
+          ),
+        ),
+         const SizedBox(height: 20,),
+         ElevatedButton(onPressed:(){
+                    print("Hi saurabh");                       // we passed call back function to this as we pressed it back the given function
+         }, child: Text("login"),
+        style: TextButton.styleFrom(
+
+        ),)
+          ],
+        ),
+       
+        )
+        
+       ],
+      )
     );
   }
 }
