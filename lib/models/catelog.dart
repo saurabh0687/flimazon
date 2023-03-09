f3a7fd014222ac897e4eb8dd587006a4c629013c
saurabh <baghelsaurabh687@gmail.com>
2023-03-09 14:03:45 +0530

    day9 completed

---
+++ lib/models/catelog.dart
@@ -0,0 +1,15 @@
+class Item{
+  final String id;
+   final String name;
+    final String desc;
+    final num price;
+    final String color;
+    final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+   
+
+}
+final products=[
+  Item(id:"2000331550094", name:"Saurabh Baghel", desc:"B.tech,CSE_IOT #rd year", price:6250, color:"Brown", image:"https://www.dreamstime.com/photos-images/nature.html")
+];
