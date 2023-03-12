5eccd2a00a7331169cc765f279b6b94a0f0a5cad
saurabh <baghelsaurabh687@gmail.com>
2023-03-12 22:32:02 +0530

    day13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,34 @@
 import 'package:flimazon/models/catelog.dart';
 import 'package:flimazon/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
 import '../widgets/item_widget.dart';
 
 //import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {  //ye ek HomePage nam ki stateless widget bn gyi jise hmne apne according bnaya hai
+class HomePage extends StatefulWidget {  //ye ek HomePage nam ki stateful widget bn gyi jise hmne apne according bnaya hai
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+  loadData() async
+  {
+   var catelogJson =await rootBundle.loadString("assets/catelog.json");
+   var decodedData=jsonDecode(catelogJson);
+  var productsData= decodedData["products"];
+  print(productsData);
+  }
   @override
   Widget build(BuildContext context) {
     final dumyList=List.generate(50,(index)=>CatelogModel.items[0]);
